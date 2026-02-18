d0b6ee5e5eda7a7b32aa570c9cbf14bd01ad2054
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-02-18 10:38:14 +0700

    feat: update Massive.get()

--- provider.py
+++ provider.py
@@ -112,15 +112,30 @@ class Massive(DataProvider):
     def __init__(self, api_key):
         super().__init__("massive", api_key)
 
+    def _get_api_timespan(self, tf: Timeframe):
+        return {
+            Timeframe.MINUTE: "minute",
+            Timeframe.HOUR: "hour",
+            Timeframe.DAY: "day",
+            Timeframe.WEEK: "week",
+            Timeframe.MONTH: "month", 
+        }[tf.unit]
+    
+    def _convert_timestamp(self, ts: pd.Timestamp):
+        res = ts.strftime("%Y-%m-%d")
+        logging.debug(f"Massive, res: {res}")
+        return res
+
     def _call_api(self, s: ForexSymbol, tf: Timeframe, time_start_utc: pd.Timestamp):
         client = RESTClient(self.api_key)
 
+        time_end_utc = pd.Timestamp.now(tz="UTC")
         aggs = list(client.list_aggs(
             ticker=f"C:{s.base}{s.quote}",
             multiplier=tf.length,
-            timespan=tf.unit,
-            from_=time_start_utc,
-            to=pd.Timestamp.now(tz="UTC"),
+            timespan=self._get_api_timespan(tf),
+            from_=self._convert_timestamp(time_start_utc),
+            to=self._convert_timestamp(time_end_utc),
             adjusted="true",
             sort="asc"
         ))
@@ -131,11 +146,13 @@ class Massive(DataProvider):
 
     def _normalize(self, aggs):
         df = pd.DataFrame(aggs)
+        logging.debug(f"df\n{df}")
         if "volume" not in df.columns:
             df["volume"] = 0
-        df.index = pd.to_datetime(df["timestamp"], unit="ms")
+        df.index = pd.to_datetime(df["timestamp"], unit="ms", utc=True)
         df.index.name = "time"
         df.drop(["vwap", "timestamp", "transactions", "otc"], axis=1, inplace=True)
+        logging.debug(f"df\n{df}")
         return df
 
 
